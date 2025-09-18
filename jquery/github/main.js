508b4ea7fbb02fcdd74c1c2096f033df7bfcf3ab
ovozzx <takiwow19@gmail.com>
2025-09-18 16:46:57 +0900

    250918

--- jquery/github/main.js
+++ jquery/github/main.js
@@ -1,6 +1,22 @@
 $().ready(function () {
   //alert("!");
   $.get("https://api.github.com/users", function (res) {
-    console.log(res);
+    var index = parseInt(Math.random() * 30);
+    console.log(index);
+    console.log(res[index]);
+    var profile = $("<img>");
+    profile.attr("src", res[index].avatar_url);
+    profile.addClass("profile");
+    $(".profile").append(profile);
+    console.log(res[index].gists_url);
+    console.log(res[index].login);
+    $.get(res[index].repos_url, function (repos_res) {
+      for (var i = 0; i < repos_res.length; i++) {
+        console.log(repos_res[i].name);
+        var reposName = $("<div>");
+        reposName.text(res[index].login + "/" + repos_res[i].name);
+        $(".aside-list").append(reposName);
+      }
+    });
   });
 });
